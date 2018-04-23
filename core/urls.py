92dffd8c9307a4bebbfff9463a90cb1ceb558bcb
Lukas Reimer <lukas.reimer@icloud.com>
2018-04-23 16:28:58 +0200

    added basic views for boards and tasks

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,10 @@ app_name = "core"
 urlpatterns = [
     path("home", views.home, name="home"),
     path("groups", views.groups, name="groups"),
-    path("boards", views.boards, name="boards"),
-    path("tasks", views.tasks, name="tasks"),
+
+    path("boards", views.board_list, name="board_list"),
+    path("board/<int:board_id>", views.board_details, name="board_details"),
+
+    path("tasks", views.task_list, name="task_list"),
+    path("task/<int:task_id>", views.task_details, name="task_details"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from . import models
 
 
 def home(request):
@@ -7,12 +7,29 @@ def home(request):
 
 
 def groups(request):
+
     return render(request, "core/groups.html", {})
 
 
-def boards(request):
-    return render(request, "core/boards.html", {})
+def board_list(request):
+    boards = get_list_or_404(models.Board)
+    return render(request, "core/board_list.html", {"boards": boards})
 
 
-def tasks(request):
-    return render(request, "core/tasks.html", {})
+def board_details(request, board_id):
+    board = get_object_or_404(models.Board, pk=board_id)
+    phases = board.phases.all().order_by("order")
+    return render(request, "core/board_details.html", {
+        "board": board,
+        "phases": phases,
+    })
+
+
+def task_list(request):
+    tasks = get_list_or_404(models.Task)
+    return render(request, "core/task_list.html", {"tasks": tasks})
+
+
+def task_details(request, task_id):
+    task = get_object_or_404(models.Task, pk=task_id)
+    return render(request, "core/task_details.html", {"task": task})
